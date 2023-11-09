0dfb58def39da845bea92ee23fe55900b0be5063
MASEOKJAE <maasj7514@gmail.com>
2023-11-10 04:44:37 +0900

    home 0.5

--- finalterm_project/lib/view/home.dart
+++ finalterm_project/lib/view/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' // new
     hide
         EmailAuthProvider,
@@ -21,195 +22,92 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   bool _isGridView = true; // true for GridView, false for ListView
 
-  // TODO: Make a collection of cards (102)
-  // List<Widget> _buildGridCards(BuildContext context) {
-  //   // List<Hotel> hotels = HotelsRepository.loadHotels();
+  StreamBuilder<QuerySnapshot> _buildGridCards(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('products').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return const CircularProgressIndicator();
 
-  //   if (hotels.isEmpty) {
-  //     return const <Card>[];
-  //   }
+        final List<DocumentSnapshot> documents = snapshot.data!.docs;
 
-  //   // products 리스트의 각 요소에 대해 map() 함수를 실행
-  //   return hotels.map((hotel) {
-  //     // 각 product에 대해 아래의 코드 블록을 실행
-  //     if (_isGridView) {
-  //       // GridView card layout
-  //       return Card(
-  //         // clipBehavior 속성은 카드의 경계를 넘어가는 자식 위젯들이 어떻게 잘릴지 결정
-  //         //  Clip.antiAlias는 경계를 부드럽게 만들어 줌
-  //         clipBehavior: Clip.antiAlias,
-  //         child: Column(
-  //           // crossAxisAlignment 속성은 수직(열) 방향으로 어떻게 정렬할지 결정 (start -> 시작 위치에서 정렬)
-  //           crossAxisAlignment: CrossAxisAlignment.start,
-  //           children: <Widget>[
-  //             // AspectRatio: 특정 비율(aspect ratio)을 유지하면서 그 안에 다른 위젯(여기서는 이미지)를 배치할 때 사용
-  //             AspectRatio(
-  //               aspectRatio: 18 / 11,
-  //               child: Image.asset(
-  //                 hotel.picture,
-  //                 fit: BoxFit.cover,
-  //               ),
-  //             ),
-  //             Expanded(
-  //               child: Padding(
-  //                 padding: const EdgeInsets.all(10),
-  //                 child: Stack(
-  //                   alignment: Alignment.bottomRight,
-  //                   children: [
-  //                     Row(
-  //                       crossAxisAlignment: CrossAxisAlignment.center,
-  //                       children: [
-  //                         const Icon(
-  //                           Icons.location_on,
-  //                           size: 17,
-  //                           color: Colors.blue,
-  //                         ),
-  //                         const SizedBox(width: 5),
-  //                         Expanded(
-  //                           child: Column(
-  //                             crossAxisAlignment: CrossAxisAlignment.end,
-  //                             children: [
-  //                               Column(
-  //                                 crossAxisAlignment: CrossAxisAlignment.start,
-  //                                 children: <Widget>[
-  //                                   // Hotel star rating with outlined stars
-  //                                   Row(
-  //                                     children: List<Widget>.generate(
-  //                                         hotel.star,
-  //                                         (index) => const Icon(Icons.star,
-  //                                             color: Colors.yellow,
-  //                                             size: 15.0)),
-  //                                     mainAxisAlignment:
-  //                                         MainAxisAlignment.start,
-  //                                   ),
-  //                                   const SizedBox(height: 2.0),
+        return GridView.builder(
+          itemCount: documents.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18 / 11,
+                    child: Image.network(
+                      documents[index]['image'],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            documents[index]['name'],
+                            style: const TextStyle(
+                                fontSize: 13, fontWeight: FontWeight.bold),
+                            maxLines: 1,
+                          ),
+                          SizedBox(height: 2.0),
+                          Text(
+                            'Price: ${documents[index]['price']}',
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                          SizedBox(height: 2.0),
+                          Text(
+                            documents[index]['Description'],
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                          SizedBox(height: 2.0),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                context,
+                                '/detail',
+                                arguments: documents[index],
+                              );
+                            },
+                            style: TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 2),
+                              minimumSize: Size.zero,
+                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            ),
+                            child: const Text('more'),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
 
-  //                                   Text(
-  //                                     hotel.name,
-  //                                     style: const TextStyle(
-  //                                         fontSize: 13,
-  //                                         fontWeight: FontWeight.bold),
-  //                                     maxLines: 1,
-  //                                   ),
-  //                                   const SizedBox(height: 2.0),
-  //                                   Text(
-  //                                     hotel.location,
-  //                                     style: const TextStyle(fontSize: 10),
-  //                                   ),
-  //                                 ],
-  //                               ),
-  //                             ],
-  //                           ),
-  //                         )
-  //                       ],
-  //                     ),
-  //                     TextButton(
-  //                       onPressed: () {
-  //                         Navigator.pushNamed(
-  //                           context,
-  //                           '/detail',
-  //                           arguments: hotel,
-  //                         );
-  //                       },
-  //                       style: TextButton.styleFrom(
-  //                         padding: const EdgeInsets.symmetric(
-  //                             horizontal: 5, vertical: 2),
-  //                         minimumSize: Size.zero,
-  //                         tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-  //                       ),
-  //                       child: const Text('more'),
-  //                     )
-  //                   ],
-  //                 ),
-  //               ),
-  //             ),
-  //           ],
-  //         ),
-  //       );
-  //     } else {
-  //       // ListView card layout
-  //       return Card(
-  //         clipBehavior: Clip.antiAlias,
-  //         child: Padding(
-  //           padding: const EdgeInsets.fromLTRB(2.0, 15.0, 2.0, 15.0),
-  //           child: Stack(
-  //             children: [
-  //               ListTile(
-  //                 leading: AspectRatio(
-  //                   aspectRatio: 1,
-  //                   child: Image.asset(
-  //                     hotel.picture,
-  //                     fit: BoxFit.cover,
-  //                   ),
-  //                 ),
-  //                 // title: Text(hotel.name),
-  //                 subtitle: Row(
-  //                   children: [
-  //                     Expanded(
-  //                       child: Column(
-  //                         crossAxisAlignment: CrossAxisAlignment.start,
-  //                         children: <Widget>[
-  //                           Row(
-  //                             children: List<Widget>.generate(
-  //                               hotel.star,
-  //                               (index) => const Icon(Icons.star,
-  //                                   color: Colors.yellow, size: 15.0),
-  //                             ),
-  //                             mainAxisAlignment: MainAxisAlignment.start,
-  //                           ),
-  //                           const SizedBox(height: 2.0),
-  //                           Text(
-  //                             hotel.name,
-  //                             style: const TextStyle(
-  //                                 fontSize: 13, fontWeight: FontWeight.bold),
-  //                             maxLines: 1,
-  //                           ),
-  //                           const SizedBox(height: 2.0),
-  //                           Text(
-  //                             hotel.location,
-  //                             style: const TextStyle(fontSize: 10),
-  //                           ),
-  //                         ],
-  //                       ),
-  //                     ),
-  //                     const SizedBox(width: 80),
-  //                   ],
-  //                 ),
-  //               ),
-  //               Positioned(
-  //                 // Positioned 위젯은 Stack 내에서 절대 위치를 지정할 수 있습니다.
-  //                 right: 15, // 오른쪽으로부터의 거리
-  //                 bottom: 2, // 아래로부터의 거리
-  //                 child: TextButton(
-  //                   onPressed: () {
-  //                     Navigator.pushNamed(context, '/detail', arguments: hotel);
-  //                   },
-  //                   style: ButtonStyle(
-  //                     padding: MaterialStateProperty.all(
-  //                         const EdgeInsets.symmetric(
-  //                             horizontal: (5), vertical: (2))),
-  //                     minimumSize: MaterialStateProperty.all(Size.zero),
-  //                     tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-  //                   ),
-  //                   child: const Text('more', style: TextStyle(fontSize: 10)),
-  //                 ),
-  //               )
-  //             ],
-  //           ),
-  //         ),
-  //       );
-  //     }
-  //   }).toList();
-  // }
-
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO: Add app bar (102)
       appBar: AppBar(
         title: const Text('Main'),
         centerTitle: true,
@@ -220,7 +118,8 @@ class _HomePageState extends State<HomePage> {
             Icons.account_circle,
             semanticLabel: 'profile',
             color: Colors.white,
-          ), onPressed: () {
+          ),
+          onPressed: () {
             Navigator.pushNamed(context, '/profile');
           },
         ),
@@ -237,63 +136,22 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
-
-      // TODO: Add a grid view (102)
-      // GridView는 count() 생성자를 호출
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ToggleButtons(
-                  onPressed: (index) {
-                    setState(() {
-                      _isGridView = index == 1;
-                    });
-                  },
-                  isSelected: [!_isGridView, _isGridView],
-                  children: const [
-                    Icon(Icons.list),
-                    Icon(Icons.grid_view),
-                  ],
-                ),
-              ],
-            ),
+          const SizedBox(height: 30,),
+          DropdownButton<String>(
+            items: <String>['A', 'B', 'C', 'D'].map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (_) {},
           ),
-          Expanded(
-            child: OrientationBuilder(
-              builder: (context, orientation) {
-                return _isGridView
-                    ? GridView.count(
-                        crossAxisCount:
-                            orientation == Orientation.portrait ? 2 : 3,
-                        padding: const EdgeInsets.all(16.0),
-                        childAspectRatio: .8,
-                        crossAxisSpacing: .5,
-                        mainAxisSpacing: .5,
-                        // children: _buildGridCards(context),
-                      )
-                    : ListView(padding: const EdgeInsets.all(16.0), children: [
-                        Container(
-                          color: Colors.red,
-                          height: 100,
-                          width: 100,
-                        ),
-                        Container(
-                          color: Colors.red,
-                          height: 100,
-                          width: 100,
-                        )
-                      ]); //_buildGridCards(context));
-              },
-            ),
-          ),
+          const SizedBox(height: 30,),
+          Expanded(child: _buildGridCards(context)),
         ],
       ),
-
-      resizeToAvoidBottomInset: false,
     );
   }
 }
